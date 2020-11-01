b12c4499d43a1f267f6e94bd83d9136c32f39723
AnshumanGo <anshuman.g.cs@gmail.com>
2020-11-01 15:20:15 +0530

    byte data type example

---
+++ src/com/practice/java/_3datatypes/_3ByteDataType.java
@@ -0,0 +1,15 @@
+package com.practice.java._3datatypes;
+
+public class _3ByteDataType {
+
+    byte byteOne = 12;
+    byte byteTwo;
+    public static void main(String[] args) {
+
+        System.out.println("value of byte data byteOne : "+new _3ByteDataType().byteOne);
+        System.out.println("value of byte data byteTwo : "+new _3ByteDataType().byteTwo);
+
+        System.out.println("Max value of byte data type : "+Byte.MAX_VALUE);
+        System.out.println("Min value of byte data type : "+Byte.MIN_VALUE);
+    }
+}
