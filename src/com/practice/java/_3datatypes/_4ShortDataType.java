f4f294f1821ece7e7fc9b41ade14cf52c6e6d20e
AnshumanGo <anshuman.g.cs@gmail.com>
2020-11-01 15:21:58 +0530

    short data type example

---
+++ src/com/practice/java/_3datatypes/_4ShortDataType.java
@@ -0,0 +1,15 @@
+package com.practice.java._3datatypes;
+
+public class _4ShortDataType {
+
+    short shortOne = 221;
+    short shortTwo;
+    public static void main(String[] args) {
+
+        System.out.println("value of short data shortOne : "+new _4ShortDataType().shortOne);
+        System.out.println("value of short data shortTwo : "+new _4ShortDataType().shortTwo);
+
+        System.out.println("Max value of short data type : "+Short.MAX_VALUE);
+        System.out.println("Min value of short data type : "+Short.MIN_VALUE);
+    }
+}
