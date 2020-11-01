d6202e06c0a3aac8e2171f77064b494a9726130f
AnshumanGo <anshuman.g.cs@gmail.com>
2020-11-01 13:47:34 +0530

    static variable example

---
+++ src/com/practice/java/_2variables/_3StaticVariable.java
@@ -0,0 +1,14 @@
+package com.practice.java._2variables;
+
+public class _3StaticVariable {
+
+    static String strVar="test";
+
+    public static void main(String[] args) {
+
+        _3StaticVariable staticVariableOne = new _3StaticVariable();
+
+        System.out.println("Static variable String strVal value for instanceVariableOne object :"+staticVariableOne.toString());
+    }
+
+}
