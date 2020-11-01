ecd6474026932cca6d449a9c700e9dce0134f275
AnshumanGo <anshuman.g.cs@gmail.com>
2020-11-01 15:26:16 +0530

    long data type example

---
+++ src/com/practice/java/_3datatypes/_6LongDataType.java
@@ -0,0 +1,15 @@
+package com.practice.java._3datatypes;
+
+public class _6LongDataType {
+
+    long longOne = 1200L;
+    long longTwo;
+    public static void main(String[] args) {
+
+        System.out.println("value of int data longOne : "+new _6LongDataType().longOne);
+        System.out.println("value of int data longOne : "+new _6LongDataType().longTwo);
+
+        System.out.println("Max value of long data type : "+Long.MAX_VALUE);
+        System.out.println("Min value of long data type : "+Long.MIN_VALUE);
+    }
+}
