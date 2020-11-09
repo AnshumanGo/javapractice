373d7bd094f0c318f1187c184a39e1250617d7e0
AnshumanGo <anshuman.g.cs@gmail.com>
2020-11-09 14:09:30 +0530

    1. updated logical AND operator example. 2. added bitwise AND operator example.

--- src/com/practice/java/_4operators/_5LogicalAndOperator.java
+++ src/com/practice/java/_4operators/_5LogicalAndOperator.java
@@ -6,8 +6,20 @@ public class _5LogicalAndOperator {
 
         int a=10,b=20 , c=5;
 
-        System.out.println(a<b&&a>c);
-        System.out.println(a<b&&a<c);
-        System.out.println(a>b&&a<c);
+        //Logical AND operator does not check both boolean condition if first condition is false
+        System.out.println(isLessThanIntValue(a,b) && isGreaterThanIntValue(a,c));
+        System.out.println(isLessThanIntValue(a,b) && isLessThanIntValue(a,c));
+        System.out.println(isGreaterThanIntValue(a,b) && isLessThanIntValue(a,c));
+        System.out.println(isGreaterThanIntValue(a,b) && isGreaterThanIntValue(a,c));
+    }
+
+    static boolean isGreaterThanIntValue( int a, int b){
+
+        return a>b;
+    }
+
+    static boolean isLessThanIntValue(int a, int b){
+
+        return a<b;
     }
 }
