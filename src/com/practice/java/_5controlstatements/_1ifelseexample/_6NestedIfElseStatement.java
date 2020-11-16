e03b48e2bed1e148529d2d82fd4f2b5bbdfb330e
AnshumanGo <anshuman.g.cs@gmail.com>
2020-11-17 02:00:04 +0530

    nested if else statement example

---
+++ src/com/practice/java/_5controlstatements/_1ifelseexample/_6NestedIfElseStatement.java
@@ -0,0 +1,21 @@
+package com.practice.java._5controlstatements._1ifelseexample;
+
+public class _6NestedIfElseStatement {
+
+    public static void main(String[] args) {
+
+        //Creating two variables for age and weight
+        boolean istwelfthPassed=true;
+        int pcmAvg=75;
+        //applying condition on age and weight
+        if(istwelfthPassed == true){
+            if(pcmAvg>75){
+                System.out.println("You can apply for BITS");
+            } else{
+                System.out.println("You can not apply for BITS");
+            }
+        } else{
+            System.out.println("You should pass 12th to apply for BITS");
+        }
+    }
+}
