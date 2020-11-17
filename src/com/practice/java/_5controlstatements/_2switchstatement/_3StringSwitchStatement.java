c871fc6a77a09885906bad545c5d604c5737debf
AnshumanGo <anshuman.g.cs@gmail.com>
2020-11-17 13:04:56 +0530

    string switch statement example

---
+++ src/com/practice/java/_5controlstatements/_2switchstatement/_3StringSwitchStatement.java
@@ -0,0 +1,21 @@
+package com.practice.java._5controlstatements._2switchstatement;
+
+public class _3StringSwitchStatement {
+
+    public static void main(String[] args) {
+
+        String classStndard = "eigth";
+
+        switch (classStndard){
+
+            case "fifth":
+            case "eighth":
+            case "tenth":
+            case "twelfth":
+                System.out.println("Board class");
+                break;
+            default:
+                System.out.println("Not board class");
+        }
+    }
+}
