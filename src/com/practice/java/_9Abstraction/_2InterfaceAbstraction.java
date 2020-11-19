61b89f2fdc7c98e9ee61f2cd3bdcd34d6745cc74
AnshumanGo <anshuman.g.cs@gmail.com>
2020-11-19 18:29:01 +0530

    interface abstraction example

---
+++ src/com/practice/java/_9Abstraction/_2InterfaceAbstraction.java
@@ -0,0 +1,50 @@
+package com.practice.java._9Abstraction;
+
+public class _2InterfaceAbstraction {
+
+    public static void main(String[] args) {
+
+        ParentInterfaceAbstractionOne parentInterfaceAbstractionOne
+                = new ChildInterfaceAbstractionTwo();
+        parentInterfaceAbstractionOne.print();
+
+        /*//ChildInterfaceAbstractionTwo is not of type ChildInterfaceAbstractionOne
+        ChildInterfaceAbstractionOne childInterfaceAbstractionOne
+                = (ChildInterfaceAbstractionOne) parentInterfaceAbstractionOne;
+        childInterfaceAbstractionOne.print();*/
+
+        /*//ChildInterfaceAbstractionTwo is not of type ChildInterfaceAbstractionOne
+        ChildInterfaceAbstractionTwo childInterfaceAbstractionTwo
+                = (ChildInterfaceAbstractionTwo) childInterfaceAbstractionOne;*/
+
+        ChildInterfaceAbstractionTwo childInterfaceAbstractionTwo
+                = (ChildInterfaceAbstractionTwo) parentInterfaceAbstractionOne;
+        childInterfaceAbstractionTwo.print();
+
+        ChildInterfaceAbstractionTwo childInterfaceAbstractionTwo2
+                = new ChildInterfaceAbstractionTwo();
+        childInterfaceAbstractionTwo2.print();
+    }
+}
+
+interface ParentInterfaceAbstractionOne{
+
+    void print();
+}
+
+/* Implement abstract method or declare class as abstract
+class ChildInterfaceAbstractionOne implements  ParentInterfaceAbstractionOne{
+
+}*/
+
+abstract class ChildInterfaceAbstractionOne implements ParentInterfaceAbstractionOne{
+    //public abstract void print();
+}
+
+class ChildInterfaceAbstractionTwo implements ParentInterfaceAbstractionOne{
+
+    public void print() {
+
+        System.out.println("Child Interface Abstraction Two print");
+    }
+}
