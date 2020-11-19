1de72634c9be1154daa9eb08fa2f0bc420f08a02
AnshumanGo <anshuman.g.cs@gmail.com>
2020-11-20 02:13:38 +0530

    java clone example

---
+++ src/com/practice/java/_12javamisc/_1JavaObjectCloning.java
@@ -0,0 +1,30 @@
+package com.practice.java._12javamisc;
+
+public class _1JavaObjectCloning implements Cloneable{
+
+    int rollNo;
+    String name;
+
+    _1JavaObjectCloning(int rollNo, String name){
+
+        this.rollNo = rollNo;
+        this.name = name;
+    }
+
+    public Object clone() throws CloneNotSupportedException{
+
+        return super.clone();
+    }
+
+    public static void main(String[] args) {
+
+        try{
+            _1JavaObjectCloning s1 = new _1JavaObjectCloning(1, "nameOne");
+
+            _1JavaObjectCloning s2 = (_1JavaObjectCloning) s1.clone();
+
+            System.out.println(s1.rollNo + " " + s1.rollNo);
+            System.out.println(s2.rollNo + " " + s2.rollNo);
+        }catch (CloneNotSupportedException exception){}
+    }
+}
