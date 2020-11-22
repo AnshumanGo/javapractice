e885455d74f1047defc34a8d0a66e8e82bfc543b
AnshumanGo <anshuman.g.cs@gmail.com>
2020-11-22 23:38:25 +0530

    java exception handling example

---
+++ src/com/practice/java/_14exceptionhandling/_1JavaException.java
@@ -0,0 +1,13 @@
+package com.practice.java._14exceptionhandling;
+
+public class _1JavaException {
+
+    public static void main(String args[]){
+        try{
+            //code that may raise exception
+            int data=100/0;
+        }catch(ArithmeticException e){System.out.println(e);}
+        //rest code of the program
+        System.out.println("rest of the code...");
+    }
+}
