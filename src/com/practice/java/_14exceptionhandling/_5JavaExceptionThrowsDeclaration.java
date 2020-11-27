3437ba5630b4c537a1614d0cc70f24b1cc2b2ee6
AnshumanGo <anshuman.g.cs@gmail.com>
2020-11-28 00:35:24 +0530

    java throws usage for checked exception example

--- src/com/practice/java/_14exceptionhandling/_5JavaExceptionThrowsDeclaration.java
+++ src/com/practice/java/_14exceptionhandling/_5JavaExceptionThrowsDeclaration.java
@@ -1,7 +1,24 @@
 package com.practice.java._14exceptionhandling;
 
+import java.io.IOException;
+
 public class _5JavaExceptionThrowsDeclaration {
 
-
+    void methodOne()throws IOException{
+        throw new IOException("method one io exception");//you should be only declaring checked exception
+    }
+    void methodTwo()throws IOException {
+        methodOne();
+    }
+    void methodThree(){
+        try{
+            methodTwo();
+        }catch(Exception e){System.out.println("method one io exception handled");}
+    }
+    public static void main(String args[]){
+        _5JavaExceptionThrowsDeclaration javaExceptionThrowsDeclaration=new _5JavaExceptionThrowsDeclaration();
+        javaExceptionThrowsDeclaration.methodThree();
+        System.out.println("Flow continues ....");
+    }
 }
 
