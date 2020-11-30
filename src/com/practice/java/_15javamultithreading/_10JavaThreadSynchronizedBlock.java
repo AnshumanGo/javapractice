1ee6fa38e0f262764afbbac9808002fb45ec4231
AnshumanGo <anshuman.g.cs@gmail.com>
2020-11-30 15:29:40 +0530

    java thread static synchronization example

--- src/com/practice/java/_15javamultithreading/_10JavaThreadSynchronizedBlock.java
+++ src/com/practice/java/_15javamultithreading/_10JavaThreadSynchronizedBlock.java
@@ -3,7 +3,9 @@ package com.practice.java._15javamultithreading;
 public class _10JavaThreadSynchronizedBlock {
 
     void printTable(int n){
-
+        /*Used to perform synchronization on any specific resource of the method.
+        If you want to synchronize only 5 lines, you can use synchronized block.
+        If you put all the codes of the method in the synchronized block, it will work same as the synchronized method.*/
         synchronized(this){//synchronized block
 
             for(int i=1;i<=10;i++){
