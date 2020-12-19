8d5bb243d33719f7793ba190498dc1986fe9c1e1
AnshumanGo <anshuman.g.cs@gmail.com>
2020-12-19 19:59:36 +0530

    1. java @SuppressWarnings(unchecked) annotation example update. 2. java collection hash map example

--- src/com/practice/java/_19javaannotations/_2JavaSuppressWarningsAnnotation.java
+++ src/com/practice/java/_19javaannotations/_2JavaSuppressWarningsAnnotation.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 public class _2JavaSuppressWarningsAnnotation {
 
+    @SuppressWarnings("unchecked")
     public static void main(String args[]){
         ArrayList list=new ArrayList();
         list.add("ram");
