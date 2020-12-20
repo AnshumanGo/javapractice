6142432a73866c678f75a5256bf2ecee15d48c09
AnshumanGo <anshuman.g.cs@gmail.com>
2020-12-21 00:03:15 +0530

    updated details of : 1. Details for Java BufferWriter And BufferReader 2. Dstails for Java BufferedOutputStream and BufferedInputStream

--- src/com/practice/java/_22javaio/_2JavaBufferWriterAndReader.java
+++ src/com/practice/java/_22javaio/_2JavaBufferWriterAndReader.java
@@ -4,7 +4,19 @@ import java.io.*;
 
 public class _2JavaBufferWriterAndReader {
 
+    /*
+    Java BufferedWriter class is used to provide buffering for Writer instances.
+    It makes the performance fast. It inherits Writer class. The buffering
+    characters are used for providing the efficient writing of single arrays,
+    characters, and strings. Java BufferedWriter extends Writer.
+
+    Java BufferedReader class is used to read the text from a character-based
+    input stream. It can be used to read data line by line by readLine() method.
+    It makes the performance fast. Java BufferedReader extends Reader.
+    */
+
     public static void main(String[] args) {
+
         try {
             FileWriter writer = new FileWriter("JavaBufferWriterAndReaderFile.txt", true);
             BufferedWriter bufferedWriter = new BufferedWriter(writer);
@@ -28,6 +40,5 @@ public class _2JavaBufferWriterAndReader {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 }
--- src/com/practice/java/_22javaio/_3JavaBufferedOutputStreamAndBufferedInputStream.java
+++ src/com/practice/java/_22javaio/_3JavaBufferedOutputStreamAndBufferedInputStream.java
@@ -7,6 +7,16 @@ import java.io.FileOutputStream;
 
 public class _3JavaBufferedOutputStreamAndBufferedInputStream {
 
+    /*
+    Java BufferedOutputStream class is used for buffering an output stream.
+    It internally uses buffer to store data. It adds more efficiency than
+    to write data directly into a stream. So, it makes the performance fast.
+    Java BufferedOutputStream extends FilterOutputStream.
+
+    Java BufferedInputStream class is used to read information from stream.
+    It internally uses buffer mechanism to make the performance fast.
+    Java BufferedInputStream extends FilterInputStream.
+    */
     public static void main(String args[])throws Exception{
         FileOutputStream fileOutputStream=new FileOutputStream("JavaBufferedOutputStreamAndBufferedInputStream.txt");
         BufferedOutputStream bufferedOutputStream=new BufferedOutputStream(fileOutputStream);
