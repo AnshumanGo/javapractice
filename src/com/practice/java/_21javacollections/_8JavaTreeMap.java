b1661a75a68820652b35fb20e615ef4c1b82b875
AnshumanGo <anshuman.g.cs@gmail.com>
2020-12-20 13:08:22 +0530

    java tree map updated description and method example

--- src/com/practice/java/_21javacollections/_8JavaTreeMap.java
+++ src/com/practice/java/_21javacollections/_8JavaTreeMap.java
@@ -20,6 +20,7 @@ public class _8JavaTreeMap {
         treeMap.put(2,"Apple");
         treeMap.put(4,"Banana");
         treeMap.put(3,"Orange");
+        treeMap.put(5,"Cherry");
 
         for(Map.Entry m:treeMap.entrySet()){
             System.out.println(m.getKey()+" "+m.getValue());
@@ -44,16 +45,26 @@ public class _8JavaTreeMap {
         System.out.println("subMap : "+treeMap.subMap(1, 2));
         //It returns the key-value pair having the least key, greater than or equal to the specified key, or null if there is no such key.
         System.out.println("ceilingEntry :"+treeMap.ceilingEntry(3));
-        //It returns the least key, greater than the specified key or null if there is no such key.
+        //It returns the key-value pair having the greatest key, lower than or equal to the specified key, or null if there is no such key.
+        System.out.println("floorEntry :"+treeMap.floorEntry(3));
+        //It returns key the least key, greater than the specified key or null if there is no such key.
         System.out.println("ceilingKey : "+treeMap.ceilingKey(3));
-        //It returns the key-value pairs whose keys are strictly less than toKey.
-        System.out.println("headMap : "+treeMap.headMap(3));
-        //It returns the least key strictly greater than the given key, or null if there is no such key.
+        //It returns the greatest key, lower than the specified key or null if there is no such key.
+        System.out.println("floorKey : "+treeMap.floorKey(3));
+        //It returns the least key-value strictly greater than the given key, or null if there is no such key.
         System.out.println("higherEntry : "+treeMap.higherEntry(3));
+        //It returns the greatest key-value strictly lower than the given key, or null if there is no such key.
+        System.out.println("lowerEntry : "+treeMap.lowerEntry(3));
+        //It returns a key-value mapping associated with the least key strictly greater than the given key, or null if there is no such key.
+        System.out.println("higherKey : "+treeMap.higherKey(3));
         //It returns a key-value mapping associated with the greatest key strictly less than the given key, or null if there is no such key.
         System.out.println("lowerKey : "+treeMap.lowerKey(3));
+        //It returns the key-value pair having the least key, or null if there is no such key
+        System.out.println("firstEntry : "+treeMap.firstEntry());
         //It returns the key-value pair having the greatest key, or null if there is no such key
         System.out.println("lastEntry : "+treeMap.lastEntry());
+        //It is used to return the first (lowest) key currently in the sorted map.
+        System.out.println("firstKey : "+treeMap.firstKey());
         //It is used to return the last (highest) key currently in the sorted map.
         System.out.println("lastKey : "+treeMap.lastKey());
         //It removes and returns a key-value mapping associated with the least key in this map, or null if the map is empty.
