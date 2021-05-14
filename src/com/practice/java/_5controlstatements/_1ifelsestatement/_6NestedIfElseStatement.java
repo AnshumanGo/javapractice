fe851319e746c801cb108663bacb89019c73e802
AnshumanGo <anshuman.g.cs@gmail.com>
2021-05-14 20:18:41 +0530

    update _6NestedIfElseStatement example

--- src/com/practice/java/_5controlstatements/_1ifelsestatement/_6NestedIfElseStatement.java
+++ src/com/practice/java/_5controlstatements/_1ifelsestatement/_6NestedIfElseStatement.java
@@ -4,12 +4,12 @@ public class _6NestedIfElseStatement {
 
     public static void main(String[] args) {
 
-        //Creating two variables for age and weight
-        boolean istwelfthPassed = true;
-        int pcmAvg = 75;
-        //applying condition on age and weight
-        if (istwelfthPassed == true) {
-            if (pcmAvg > 75) {
+        int physicsMarks=34, chemistryMarks=35, mathematicsMarks=36, englishMarks=35, hindiMarks=35;
+        int avgMarks = (physicsMarks+chemistryMarks+mathematicsMarks+englishMarks+hindiMarks)/5;
+        int pcmAvg=(physicsMarks+chemistryMarks+mathematicsMarks)/3;
+
+        if (physicsMarks>=35 && chemistryMarks>=35 && mathematicsMarks>=35 && englishMarks>=35 && hindiMarks>=35 && avgMarks>=35) {
+            if (pcmAvg >= 75) {
                 System.out.println("You can apply for BITS");
             } else {
                 System.out.println("You can not apply for BITS");
